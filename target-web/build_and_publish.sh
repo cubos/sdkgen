e7481401b2c81fb54f25245f87f3029cc5eb890d
Guilherme Bernal <dev@lbguilherme.com>
2017-03-07 09:17:57 -0300

    Fix build_and_publish.sh

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -4,8 +4,8 @@ VERSION=1.0.$(git rev-list --count HEAD)
 cp $1 /root/api.ts
 cd /root
 
-npm-exec json -I -f package.json -e 'this.name=\"@cubos/'$NAME'\"'
+npm exec json -I -f package.json -e 'this.name=\"@cubos/'$NAME'\"'
 npm version $VERSION || true
-npm-exec tsc
-npm-exec babel api.js -o api.js
+npm exec tsc
+npm exec babel api.js -o api.js
 npm publish
