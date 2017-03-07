722d9afe148f96cd02242982efc39b774fac9daa
Guilherme Bernal <dev@lbguilherme.com>
2017-03-07 08:47:20 -0300

    target-web: fix build script

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -1,3 +1,10 @@
+NAME=$(basename `git rev-parse --show-toplevel`)
+VERSION=1.0.$(git rev-list --count HEAD)
+
 cp $1 /root
 cd /root
-npm run build
+
+npm-exec json -I -f package.json -e 'this.name=\"@cubos/'$NAME'\"'
+npm version $VERSION || true
+npm-exec tsc
+npm-exec babel api.js -o api.js
