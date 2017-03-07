02a4ef4e4c2a7c43b39a1b33fb93569399c56a86
Guilherme Bernal <dev@lbguilherme.com>
2017-03-07 09:52:54 -0300

    build npm fixes

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -1,11 +1,15 @@
+#!/usr/bin/env bash
+
+export PATH=$(npm bin):$PATH
+
 NAME=$(basename `git rev-parse --show-toplevel`)
 VERSION=1.0.$(git rev-list --count HEAD)
 
 cp $1 /root/api.ts
 cd /root
 
-npm exec json -I -f package.json -e 'this.name=\"@cubos/'$NAME'\"'
+json -I -f package.json -e 'this.name=\"@cubos/'$NAME'\"'
 npm version $VERSION || true
-npm exec tsc
-npm exec babel api.js -o api.js
+tsc
+babel api.js -o api.js
 npm publish
