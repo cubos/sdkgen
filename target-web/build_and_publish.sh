36cfb12e405f56e016356c4592dcfdc62716059e
Guilherme Bernal <dev@lbguilherme.com>
2017-03-07 09:56:00 -0300

    fix again npm

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-export PATH=$(npm bin):$PATH
-
 NAME=$(basename `git rev-parse --show-toplevel`)
 VERSION=1.0.$(git rev-list --count HEAD)
 
 cp $1 /root/api.ts
 cd /root
 
+export PATH=$(npm bin):$PATH
 json -I -f package.json -e 'this.name=\"@cubos/'$NAME'\"'
 npm version $VERSION || true
 tsc
