aabe44235479d6b6602e3c3f42534fe87171cfa9
Guilherme Bernal <dev@lbguilherme.com>
2017-03-07 09:58:34 -0300

    fix again

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -7,7 +7,7 @@ cp $1 /root/api.ts
 cd /root
 
 export PATH=$(npm bin):$PATH
-json -I -f package.json -e 'this.name=\"@cubos/'$NAME'\"'
+json -I -f package.json -e 'this.name="@cubos/'$NAME'"'
 npm version $VERSION || true
 tsc
 babel api.js -o api.js
