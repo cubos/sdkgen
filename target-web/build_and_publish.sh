67728efb9ebfb381247aa7b83d25fe89806981fa
Guilherme Bernal <dev@lbguilherme.com>
2017-03-09 09:32:19 -0300

    Add debug to target-web

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -5,10 +5,12 @@ VERSION=1.0.$(git rev-list --count HEAD)
 
 cp $1 /root/stage/api.ts
 cd /root/stage
-
+ls -lA
 export PATH=$(npm bin):$PATH
 json -I -f package.json -e 'this.name="@cubos/'$NAME'"'
 npm version $VERSION || true
 tsc
 babel api.js -o api.js
 npm publish
+pwd
+ls -lA
