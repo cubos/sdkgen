e81ee55545326d4cbca862188bd57d071f1f5b03
Guilherme Bernal <dev@lbguilherme.com>
2017-03-09 09:35:15 -0300

    Fix: add missing npmignore

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -5,12 +5,10 @@ VERSION=1.0.$(git rev-list --count HEAD)
 
 cp $1 /root/stage/api.ts
 cd /root/stage
-ls -lA
+
 export PATH=$(npm bin):$PATH
 json -I -f package.json -e 'this.name="@cubos/'$NAME'"'
 npm version $VERSION || true
 tsc
 babel api.js -o api.js
 npm publish
-pwd
-ls -lA
