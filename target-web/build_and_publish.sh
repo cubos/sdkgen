9f8bf16d4c6828e1a47f13ceae7854d4c85a1cee
Guilherme Bernal <dev@lbguilherme.com>
2017-03-09 08:55:05 -0300

    Dont build from home

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -3,8 +3,8 @@
 NAME=$(basename `git rev-parse --show-toplevel`)
 VERSION=1.0.$(git rev-list --count HEAD)
 
-cp $1 /root/api.ts
-cd /root
+cp $1 /root/stage/api.ts
+cd /root/stage
 
 export PATH=$(npm bin):$PATH
 json -I -f package.json -e 'this.name="@cubos/'$NAME'"'
