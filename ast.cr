15a993062207a823e83e9b51b93617860382d528
Guilherme Bernal <dev@lbguilherme.com>
2017-03-13 16:06:40 -0300

    Check AST for semantic errors

--- ast.cr
+++ ast.cr
@@ -4,6 +4,8 @@ module AST
   end
 
   abstract class PrimitiveType < Type
+    def check(ast)
+    end
   end
 
   class StringPrimitiveType < PrimitiveType
@@ -37,11 +39,19 @@ module AST
     property base
     def initialize(@base : Type)
     end
+
+    def check(ast)
+      @base.check(ast)
+    end
   end
 
   class ArrayType < Type
     property base
     def initialize(@base : Type)
+    end
+
+    def check(ast)
+      @base.check(ast)
     end
   end
 
@@ -50,6 +60,11 @@ module AST
     property operations = [] of Operation
     property options = Options.new
     property errors = [] of String
+
+    def check
+      custom_types.each &.check(self)
+      operations.each &.check(self)
+    end
   end
 
   class Options
@@ -60,16 +75,30 @@ module AST
     property! name : String
     property! type : Type
     property secret = false
+
+    def check(ast)
+      type.check(ast)
+    end
   end
 
   class CustomType
     property! name : String
     property fields = [] of Field
+
+    def check(ast)
+      fields.each &.check(ast)
+    end
   end
 
   class CustomTypeReference < Type
     property name
     def initialize(@name : String)
+    end
+
+    def check(ast)
+      unless ast.custom_types.find {|t| t.name == name }
+        raise "Could not find type '#{name}'"
+      end
     end
   end
 
@@ -77,6 +106,10 @@ module AST
     property! name : String
     property args = [] of Field
     property! return_type : Type
+
+    def check(ast)
+      args.each &.check(ast)
+    end
 
     def fnName
       name
