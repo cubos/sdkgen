684bab0800aa8b81ea89c2e11a0cc8b0f00c29de
Guilherme Bernal <dev@lbguilherme.com>
2017-03-16 15:00:57 -0300

    io target

--- target_swift_ios.cr
+++ target_swift_ios.cr
@@ -19,10 +19,15 @@ END
     # }
     @ast.operations.each do |op|
       args = op.args.map {|arg| "#{arg.name}: #{native_type arg.type}" }
+
       if op.return_type.is_a? AST::VoidPrimitiveType
         args << "callback: @escaping (_ error: APIInternal.Error?) -> Void"
       else
-        args << "callback: @escaping (_ result: #{native_type op.return_type}, _ error: APIInternal.Error?) -> Void"
+        ret = op.return_type
+        unless ret.is_a? AST::OptionalType
+          ret = AST::OptionalType.new ret
+        end
+        args << "callback: @escaping (_ result: #{native_type ret}, _ error: APIInternal.Error?) -> Void"
       end
       @io << ident(String.build do |io|
         io << "static public func #{op.fnName}(#{args.join(", ")}) {\n"
@@ -36,11 +41,11 @@ END
       end
           io << <<-END
 
-APIInternal.makeRequest(#{op.fnName.inspect}, args) {(result: AnyObject?, error: APIInternal.Error?) -> Void in
-    if error {
-        callback(nil, error);
+APIInternal.makeRequest(#{op.fnName.inspect}, args) { result, error in
+    if error != nil {
+        callback(#{"nil, " unless op.return_type.is_a? AST::VoidPrimitiveType}error);
     } else {
-        callback.onResult(#{"#{type_from_json op.return_type, "result"}, " unless op.return_type.is_a? AST::VoidPrimitiveType}nil);
+        callback(#{"#{type_from_json op.return_type, "result"}, " unless op.return_type.is_a? AST::VoidPrimitiveType}nil);
     }
 }
 
@@ -61,13 +66,13 @@ class APIInternal {
         var type: String
         var message: String
 
-        init(type: String, message: String) {
+        init(_ type: String, _ message: String) {
             self.type = type
             self.message = message
         }
     }
 
-    static func device() {
+    static func device() -> NSDictionary {
         let device = NSMutableDictionary()
         device["platform"] = "ios"
         device["fingerprint"] = "."
@@ -77,74 +82,73 @@ class APIInternal {
         } else {
             device["version"] = "unknown"
         }
-        device["language"] = NSLocale.preferredLanguages[0]
+        device["language"] = Locale.preferredLanguages[0]
         if let deviceId = UserDefaults.standard.value(forKey: "device-id") as? String {
             device["id"] = deviceId
         }
         return device
     }
 
-    static func randomBytesHex(len: Int32) {
-        var randomBytes = [UInt8](count: len, repeatedValue: 0)
-        SecRandomCopyBytes(kSecRandomDefault, len, &randomBytes)
-        return randomBytes.map({String(format: "%02hhx", $0)}).joinWithSeparator("")
+    static func randomBytesHex(len: Int) -> String {
+        var randomBytes = [UInt8](repeating: 0, count: len)
+        let _ = SecRandomCopyBytes(kSecRandomDefault, len, &randomBytes)
+        return randomBytes.map({String(format: "%02hhx", $0)}).joined(separator: "")
     }
 
-    static func decodeDate(str: String) -> NSDate {
-        let formatter = NSDateFormatter()
-        formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
-        formatter.timeZone = NSTimeZone(name: "UTC")
-        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    static func decodeDate(str: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd"
-        return formatter.dateFromString(str)!
+        return formatter.date(from: str)!
     }
 
-    static func encodeDate(date: NSDate) -> String {
-        let formatter = NSDateFormatter()
-        formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
-        formatter.timeZone = NSTimeZone(name: "UTC")
-        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    static func encodeDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd"
-        return formatter.stringFromDate(date)
+        return formatter.string(from: date)
     }
 
-    static func decodeDateTime(str: String) -> NSDate {
-        let formatter = NSDateFormatter()
-        formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
-        formatter.timeZone = NSTimeZone(name: "UTC")
-        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    static func decodeDateTime(str: String) -> Date {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-        return formatter.dateFromString(str)!
+        return formatter.date(from: str)!
     }
 
-    static func encodeDateTime(date: NSDate) -> String {
-        let formatter = NSDateFormatter()
-        formatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
-        formatter.timeZone = NSTimeZone(name: "UTC")
-        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+    static func encodeDateTime(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-        return formatter.stringFromDate(date)
+        return formatter.string(from: date)
     }
 
-    static func makeRequest(name: String, args: NSDictionary, callback: (result: AnyObject?, error: Error?) -> Void) {
+    static func makeRequest(_ name: String, _ args: NSDictionary, callback: @escaping (_ result: AnyObject?, _ error: Error?) -> Void) {
         let body = [
-            "id": randomBytesHex(16),
+            "id": randomBytesHex(len: 16),
             "device": device(),
             "name": name,
             "args": args
-        ]
+        ] as [String : Any]
 
-        Alamofire.request(.POST, "https://\(baseUrl)/\(name)", parameters: body, encoding: ParameterEncoding.JSON, headers: header).validate().responseJSON { (response) in
+        Alamofire.request("https://\\(baseUrl)/\\(name)", method: .post, parameters: body).validate().responseJSON { response in
             switch response.result {
-            case .Failure(let err):
-                callback(nil, Error("Connection", err))
+            case .failure(let err):
+                callback(nil, Error("Connection", err.localizedDescription))
                 break
-            case .Success(let body):
-                if body["ok"] as Bool {
-                    callback(result: body["result"], error: nil)
+            case .success(let value):
+                let body = value as! [String: AnyObject?]
+                if body["ok"] as! Bool {
+                    callback(body["result"]!, nil)
                 } else {
-                    let error = body["error"] as! NSDictionary
-                    callback(result: nil, error: Error(error["type"], error["message"]))
+                    let error = body["error"] as! [String: String]
+                    callback(nil, Error(error["type"]!, error["message"]!))
                 }
                 break
             }
