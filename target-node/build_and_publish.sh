cf695d5237c9483274afe00d950feef98338e311
Roberto <iseme.beto@gmail.com>
2017-04-10 11:00:36 -0300

    add target-node

---
+++ target-node/build_and_publish.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+NAME=$2
+VERSION=1.0.$3
+
+cp $1 /root/stage/api.ts
+cd /root/stage
+
+export PATH=$(npm bin):$PATH
+json -I -f package.json -e 'this.name="@cubos/'$NAME'"'
+npm version $VERSION || true
+tsc
+babel api.js -o api.js
+npm publish
