66a8852e064bfa9925c43dd0c395545a2c52dd91
Guilherme Bernal <dev@lbguilherme.com>
2017-04-17 14:35:59 -0300

    Fix moment import

--- target_typescript_nodeserver.cr
+++ target_typescript_nodeserver.cr
@@ -6,7 +6,7 @@ class TypeScriptServerTarget < TypeScriptTarget
 import http from "http";
 import crypto from "crypto";
 import os from "os";
-import moment from "moment";
+import * as moment from "moment";
 import r from "../rethinkdb";
 
 
