fc00698c32fa5445ec88851f344d2947ff70102c
Guilherme Bernal <dev@lbguilherme.com>
2017-06-22 09:37:10 -0300

    Allow method put

--- target_typescript_nodeserver.cr
+++ target_typescript_nodeserver.cr
@@ -106,7 +106,7 @@ export function start(port: number) {
     });
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Max-Age", "86400");
     res.setHeader("Content-Type", "application/json");
