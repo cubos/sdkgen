5a16c3262f21fa2803ee13f3b9e0db1d7c51bab3
Victor Magalhães <victor@cubostecnologia.com.br>
2017-10-20 15:44:34 -0300

    Log the shit all over

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -9,6 +9,15 @@ cd /root/stage
 export PATH=$(npm bin):$PATH
 json -I -f package.json -e 'this.name="@cubos/'$NAME'"'
 npm version $VERSION || true
+ls
+echo "tsc will run"
 tsc
+echo "tsc runned"
+ls
+echo "babel will run"
 babel api.js -o api.js
+echo "babel runned"
+ls
+echo "YAY"
+cat .gitignore
 npm publish
