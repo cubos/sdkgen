5237a9103a87df58ad27bb9c606e4ade01f49c48
Guilherme Bernal <dev@lbguilherme.com>
2017-10-21 18:59:45 -0300

    Fully rewrite Lexer (using TDD!)

--- lexer.cr
+++ lexer.cr
@@ -6,192 +6,170 @@ class Lexer
   class LexerException < Exception
   end
 
-  def initialize(@filename : String)
-    @raw = File.read(filename)
-    @start = 0
-    @pos = 0
-    @line = 1
-    @col = 0
+  @start_pos = 0
+  @start_line = 1
+  @start_column = 1
+  @line = 1
+  @column = 1
+
+  def initialize(string : String, filename : String? = nil)
+    @filename = filename || "-"
+    @reader = Char::Reader.new(string)
   end
 
-  def initialize(io : IO)
-    @filename = "<noname>"
-    @raw = io.gets_to_end
-    @start = 0
-    @pos = 0
-    @line = 1
-    @col = 0
+  private def pos
+    @reader.pos
   end
 
   private def current_char
-    @raw[@pos]?
-  end
-
-  private def current_char!
-    @raw[@pos]
-  end
-
-  private def peek_next_char
-    @raw[@pos+1]?
+    @reader.current_char
   end
 
   private def next_char
-    @pos += 1
-    current_char
+    @reader.next_char
+    @column += 1
+    @reader.current_char
+  end
+
+  private def substring(start_pos, end_pos)
+    reader = Char::Reader.new(@reader.string)
+    reader.pos = start_pos
+    String.build do |io|
+      while reader.pos <= end_pos
+        io << reader.current_char
+        reader.next_char
+      end
+    end
   end
 
   def next_token
-    skip
-    start_token
+    @start_pos = @reader.pos
+    @start_line = @line
+    @start_column = @column
+    token = nil
 
-    return nil unless current_char
-
-    return make_token(CurlyOpenSymbolToken.new) if literal_match("{")
-    return make_token(CurlyCloseSymbolToken.new) if literal_match("}")
-    return make_token(ParensOpenSymbolToken.new) if literal_match("(")
-    return make_token(ParensCloseSymbolToken.new) if literal_match(")")
-    return make_token(OptionalSymbolToken.new) if literal_match("?")
-    return make_token(ArraySymbolToken.new) if literal_match("[]")
-    return make_token(ColonSymbolToken.new) if literal_match(":")
-    return make_token(ExclamationMarkSymbolToken.new) if literal_match("!")
-    return make_token(CommaSymbolToken.new) if literal_match(",")
-    return make_token(EqualSymbolToken.new) if literal_match("=")
-
-    return make_token(ImportKeywordToken.new) if word_match("import")
-    return make_token(TypeKeywordToken.new) if word_match("type")
-    return make_token(EnumKeywordToken.new) if word_match("enum")
-    return make_token(GetKeywordToken.new) if word_match("get")
-    return make_token(FunctionKeywordToken.new) if word_match("function")
-    return make_token(SubscribeKeywordToken.new) if word_match("subscribe")
-    return make_token(ErrorKeywordToken.new) if word_match("error")
-    return make_token(PrimitiveTypeToken.new("bool")) if word_match("bool")
-    return make_token(PrimitiveTypeToken.new("int")) if word_match("int")
-    return make_token(PrimitiveTypeToken.new("uint")) if word_match("uint")
-    return make_token(PrimitiveTypeToken.new("float")) if word_match("float")
-    return make_token(PrimitiveTypeToken.new("string")) if word_match("string")
-    return make_token(PrimitiveTypeToken.new("datetime")) if word_match("datetime")
-    return make_token(PrimitiveTypeToken.new("date")) if word_match("date")
-    return make_token(PrimitiveTypeToken.new("bytes")) if word_match("bytes")
-    return make_token(PrimitiveTypeToken.new("void")) if word_match("void")
-
-    if current_char!.letter?
-      while current_char && (current_char!.letter? || current_char!.number?)
-        next_char
-      end
-      return make_token(IdentifierToken.new(@raw[@start...@pos]))
-    end
-
-    if current_char! == '$'
+    case current_char
+    when '\0'
+      return nil
+    when ' ', '\n'
       next_char
-      while current_char && (current_char!.letter? || current_char!.number?)
-        next_char
-      end
-      return make_token(GlobalOptionToken.new(@raw[@start+1...@pos]))
-    end
-
-    if current_char! == '"'
-      return string_match
-    end
-
-    until [' ', '\t', '\r', '\n'].includes? current_char
-      next_char
-    end
-
-    raise LexerException.new("Invalid token at #{@filename}:#{@line}:#{@col}: \"#{@raw[@start...@pos]}\"")
-  end
-
-  private def skip
-    while true
-      if [' ', '\t', '\r', '\n'].includes? current_char
-        next_char
-        next
-      end
-
-      if current_char == '/' && peek_next_char == '/'
+      return next_token
+    when '/'
+      case next_char
+      when '/'
         while true
-          break if next_char == '\n'
+          case next_char
+          when '\0'
+            return nil
+          when '\n'
+            next_char
+            return next_token
+          end
         end
-        next_char
-        next
       end
-
-      break
-    end
-  end
-
-  private def start_token
-    while @start < @pos
-      if @raw[@start] == '\n'
-        @line += 1
-        @col = 0
-      else
-        @col += 1
-      end
-      @start += 1
-    end
-  end
-
-  private def make_token(token)
-    token.filename = @filename
-    token.line = @line
-    token.col = @col
-    token
-  end
-
-  private def literal_match(str : String)
-    str.each_char.with_index.each do |c, i|
-      return false unless @raw[@pos+i]? == c
-    end
-
-    @pos += str.size
-  end
-
-  private def word_match(str : String)
-    after = @raw[@pos+str.size]?
-    return false if after && after.letter?
-    return literal_match(str)
-  end
-
-  private def string_match
-    unless current_char != "\""
-      raise LexerException.new("BUG: Expected string start here")
-    end
-
-    next_char
-    contents = String::Builder.new;
-    while true
-      c = current_char
-      unless c
-        raise LexerException.new("Unexpected end of input inside string literal at #{@filename}:#{@line}:#{@col}")
-      end
-      if c == '"'
-        next_char
-        break
-      end
-      if c == '\\'
-        next_char
-        case current_char
-        when 'n'
-          contents << '\n'
-        when 'r'
-          contents << '\r'
-        when 't'
-          contents << '\t'
-        when '"'
-          contents << '"'
-        when '\\'
-          contents << '\\'
-        when nil
-          raise LexerException.new("Unexpected end of input on escape sequence inside string literal at #{@filename}:#{@line}:#{@col}")
-        else
-          contents << current_char!
-        end
-        next_char
-        next
-      end
-      contents << c
+    when '{'
       next_char
+      token = CurlyOpenSymbolToken.new
+    when '}'
+      next_char
+      token = CurlyCloseSymbolToken.new
+    when '('
+      next_char
+      token = ParensOpenSymbolToken.new
+    when ')'
+      next_char
+      token = ParensCloseSymbolToken.new
+    when '?'
+      next_char
+      token = OptionalSymbolToken.new
+    when ':'
+      next_char
+      token = ColonSymbolToken.new
+    when '!'
+      next_char
+      token = ExclamationMarkSymbolToken.new
+    when ','
+      next_char
+      token = CommaSymbolToken.new
+    when '='
+      next_char
+      token = EqualSymbolToken.new
+    when '['
+      case next_char
+      when ']'
+        next_char
+        token = ArraySymbolToken.new
+      end
+    when '$'
+      next_char
+      if current_char.ascii_letter?
+        while current_char.ascii_letter? || current_char.ascii_number?
+          next_char
+        end
+
+        token = GlobalOptionToken.new(substring(@start_pos+1, pos-1))
+      end
+    when '"'
+      builder = String::Builder.new
+      while true
+        case next_char
+        when '\0'
+          break
+        when '\\'
+          case next_char
+          when '\0'
+            break
+          when 'n'; builder << '\n'
+          when 't'; builder << '\t'
+          when '"'; builder << '"'
+          when '\\'; builder << '\\'
+          else
+            builder << current_char
+          end
+        when '"'
+          next_char
+          token = StringLiteralToken.new(builder.to_s)
+          break
+        else
+          builder << current_char
+        end
+      end
+    else
+      if current_char.ascii_letter?
+        next_char
+        while current_char.ascii_letter? || current_char.ascii_number?
+          next_char
+        end
+
+        str = substring(@start_pos, pos-1)
+
+        token = case str
+        when "error"; ErrorKeywordToken.new
+        when "enum"; EnumKeywordToken.new
+        when "type"; TypeKeywordToken.new
+        when "import"; ImportKeywordToken.new
+        when "get"; GetKeywordToken.new
+        when "function"; FunctionKeywordToken.new
+        when "subscribe"; SubscribeKeywordToken.new
+        when "bool", "int", "uint", "float", "string", "date", "datetime", "bytes", "void"
+          PrimitiveTypeToken.new(str)
+        else
+          IdentifierToken.new(str)
+        end
+      end
     end
-    return make_token(StringLiteralToken.new(contents.to_s))
+
+    if token
+      token.filename = @filename
+      token.line = @start_line
+      token.column = @start_column
+      return token
+    else
+      if current_char != '\0'
+        raise LexerException.new("Unexpected character #{current_char.inspect} at #{@filename}:#{@line}:#{@column}")
+      else
+        raise LexerException.new("Unexpected end of file at #{@filename}")
+      end
+    end
   end
 end
