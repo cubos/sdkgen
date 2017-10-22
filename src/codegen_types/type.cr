88e8181093697f74f22aaeb854ac3daab5d40a45
Guilherme Bernal <dev@lbguilherme.com>
2017-10-22 18:41:52 -0300

    Remove base codegen methods

--- src/codegen_types/type.cr
+++ src/codegen_types/type.cr
@@ -1,15 +1,4 @@
 module AST
   class Type
-    def typescript_decode(expr)
-      raise "TODO: #{self.class}"
-    end
-
-    def typescript_encode(expr)
-      raise "TODO: #{self.class}"
-    end
-
-    def typescript_native_type
-      raise "TODO: #{self.class}"
-    end
   end
 end
