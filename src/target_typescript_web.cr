21cf381ba56dee04c7246ceaf8fe09837d5218ee
Guilherme Bernal <dev@lbguilherme.com>
2017-10-22 18:56:47 -0300

    Move struct and enum declaration to type fiels

--- src/target_typescript_web.cr
+++ src/target_typescript_web.cr
@@ -16,12 +16,12 @@ export function setStaging(use: boolean) {
 END
 
     @ast.struct_types.each do |t|
-      @io << generate_struct_type(t)
+      @io << t.typescript_definition
       @io << "\n\n"
     end
 
     @ast.enum_types.each do |t|
-      @io << generate_enum_type(t)
+      @io << t.typescript_definition
       @io << "\n\n"
     end
 
