0a24519dbe8c4869420e50ed99d63d2e01b2f286
Guilherme Bernal <dev@lbguilherme.com>
2017-10-25 14:54:18 -0300

    Remove normalize

--- src/codegen_types/string.cr
+++ src/codegen_types/string.cr
@@ -1,11 +1,13 @@
 module AST
   class StringPrimitiveType
     def typescript_decode(expr)
-      "#{expr}.normalize()"
+      "#{expr}"
+      # "#{expr}.normalize()"
     end
 
     def typescript_encode(expr)
-      "#{expr}.normalize()"
+      "#{expr}"
+      # "#{expr}.normalize()"
     end
 
     def typescript_native_type
