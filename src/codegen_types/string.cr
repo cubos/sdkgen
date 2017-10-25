aaa2402e35fdfd5c734b0204dac5b16827c44045
Guilherme Bernal <dev@lbguilherme.com>
2017-10-25 13:48:10 -0300

    add normalize back

--- src/codegen_types/string.cr
+++ src/codegen_types/string.cr
@@ -1,11 +1,11 @@
 module AST
   class StringPrimitiveType
     def typescript_decode(expr)
-      "#{expr}"
+      "#{expr}.normalize()"
     end
 
     def typescript_encode(expr)
-      "#{expr}"
+      "#{expr}.normalize()"
     end
 
     def typescript_native_type
