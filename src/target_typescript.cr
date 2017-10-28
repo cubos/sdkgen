f76f762278d937bbdeac71072c44e5ce6f9c4289
Guilherme Bernal <dev@lbguilherme.com>
2017-10-28 08:44:25 -0300

    Remove operation_ret from typescript

--- src/target_typescript.cr
+++ src/target_typescript.cr
@@ -1,16 +1,12 @@
 require "./target"
 
 abstract class TypeScriptTarget < Target
-  def operation_ret(op : AST::GetOperation | AST::FunctionOperation)
-    op.return_type.is_a?(AST::VoidPrimitiveType) ? "void" : op.return_type.typescript_native_type
-  end
-
   def operation_args(op : AST::Operation)
     args = op.args.map { |arg| "#{arg.name}: #{arg.type.typescript_native_type}" }
     "(#{args.join(", ")})"
   end
 
   def operation_type(op : AST::Operation)
-    "#{operation_args(op)} => Promise<#{operation_ret(op)}>"
+    "#{operation_args(op)} => Promise<#{op.return_type.typescript_native_type}>"
   end
 end
