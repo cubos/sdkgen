2f3a4685d0fa546f69562f6684191db0d1a8fc58
Guilherme Bernal <dev@lbguilherme.com>
2017-11-06 13:05:51 -0300

    failtypecheck inline for better error stack trace

--- src/codegen_types/int.cr
+++ src/codegen_types/int.cr
@@ -15,7 +15,8 @@ module AST
     def typescript_check_encoded(expr, descr)
       String.build do |io|
         io << "if (typeof #{expr} !== \"number\" || (#{expr} | 0) !== #{expr}) {\n"
-        io << "    failTypeCheck(#{descr}, ctx);\n"
+        io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
+        io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1);\n"
         io << "}\n"
       end
     end
@@ -23,7 +24,8 @@ module AST
     def typescript_check_decoded(expr, descr)
       String.build do |io|
         io << "if (typeof #{expr} !== \"number\" || (#{expr} | 0) !== #{expr}) {\n"
-        io << "    failTypeCheck(#{descr}, ctx);\n"
+        io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
+        io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1);\n"
         io << "}\n"
       end
     end
--- src/codegen_types/phone.cr
+++ src/codegen_types/phone.cr
@@ -15,7 +15,8 @@ module AST
     def typescript_check_encoded(expr, descr)
       String.build do |io|
         io << "if (typeof #{expr} !== \"string\" || #{expr}.replace(/[^0-9+]/g, \"\").length < 5 || #{expr}[0] !== \"+\") {\n"
-        io << "    failTypeCheck(#{descr}, ctx);\n"
+        io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
+        io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1);\n"
         io << "}\n"
       end
     end
@@ -23,7 +24,8 @@ module AST
     def typescript_check_decoded(expr, descr)
       String.build do |io|
         io << "if (typeof #{expr} !== \"string\" || #{expr}.replace(/[^0-9+]/g, \"\").length < 5 || #{expr}[0] !== \"+\") {\n"
-        io << "    failTypeCheck(#{descr}, ctx);\n"
+        io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
+        io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1);\n"
         io << "}\n"
       end
     end
