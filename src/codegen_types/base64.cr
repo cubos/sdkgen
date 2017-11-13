6a22618ea829d2d3386ddf93381fdb1e27a1a437
Guilherme Bernal <dev@lbguilherme.com>
2017-11-13 10:28:39 -0300

    Add checks for non null types

--- src/codegen_types/base64.cr
+++ src/codegen_types/base64.cr
@@ -14,7 +14,7 @@ module AST
 
     def typescript_check_encoded(expr, descr)
       String.build do |io|
-        io << "if (typeof #{expr} !== \"string\" || !#{expr}.match(/^(?:[A-Za-z0-9+\/]{4}\\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)$/)) {\n"
+        io << "if (#{expr} === null || #{expr} === undefined || typeof #{expr} !== \"string\" || !#{expr}.match(/^(?:[A-Za-z0-9+\/]{4}\\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)$/)) {\n"
         io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
         io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1000);\n"
         io << "}\n"
@@ -23,7 +23,7 @@ module AST
 
     def typescript_check_decoded(expr, descr)
       String.build do |io|
-        io << "if (typeof #{expr} !== \"string\" || !#{expr}.match(/^(?:[A-Za-z0-9+\/]{4}\\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)$/)) {\n"
+        io << "if (#{expr} === null || #{expr} === undefined || typeof #{expr} !== \"string\" || !#{expr}.match(/^(?:[A-Za-z0-9+\/]{4}\\n?)*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)$/)) {\n"
         io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
         io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1000);\n"
         io << "}\n"
--- src/codegen_types/date.cr
+++ src/codegen_types/date.cr
@@ -14,7 +14,7 @@ module AST
 
     def typescript_check_encoded(expr, descr)
       String.build do |io|
-        io << "if (typeof #{expr} !== \"string\" || !#{expr}.match(/^[0-9]{4}-[01][0-9]-[0123][0-9]$/)) {\n"
+        io << "if (#{expr} === null || #{expr} === undefined || typeof #{expr} !== \"string\" || !#{expr}.match(/^[0-9]{4}-[01][0-9]-[0123][0-9]$/)) {\n"
         io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
         io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1000);\n"
         io << "}\n"
@@ -23,7 +23,7 @@ module AST
 
     def typescript_check_decoded(expr, descr)
       String.build do |io|
-        io << "if (!(#{expr} instanceof Date)) {\n"
+        io << "if (#{expr} === null || #{expr} === undefined || !(#{expr} instanceof Date)) {\n"
         io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
         io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1000);\n"
         io << "}\n"
