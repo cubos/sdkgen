c6f1337d632f89dfccc99cb9da66bef5722fbde9
Guilherme Bernal <dev@lbguilherme.com>
2017-11-13 10:28:39 -0300

    Add checks for non null types

--- src/codegen_types/uuid.cr
+++ src/codegen_types/uuid.cr
@@ -14,7 +14,7 @@ module AST
 
     def typescript_check_encoded(expr, descr)
       String.build do |io|
-        io << "if (typeof #{expr} !== \"string\" || !#{expr}.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/)) {\n"
+        io << "if (#{expr} === null || #{expr} === undefined || typeof #{expr} !== \"string\" || !#{expr}.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/)) {\n"
         io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
         io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1000);\n"
         io << "}\n"
@@ -23,7 +23,7 @@ module AST
 
     def typescript_check_decoded(expr, descr)
       String.build do |io|
-        io << "if (typeof #{expr} !== \"string\" || !#{expr}.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/)) {\n"
+        io << "if (#{expr} === null || #{expr} === undefined || typeof #{expr} !== \"string\" || !#{expr}.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/)) {\n"
         io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
         io << "    setTimeout(() => captureError(err, ctx.req, ctx.call), 1000);\n"
         io << "}\n"
