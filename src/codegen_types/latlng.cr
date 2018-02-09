a42b4f7154bb1c8c30c850c206730f7c9420cf2a
Guilherme Bernal <dev@lbguilherme.com>
2018-02-09 15:50:41 -0300

    improve strict check description

--- src/codegen_types/latlng.cr
+++ src/codegen_types/latlng.cr
@@ -23,7 +23,7 @@ module AST
     def typescript_check_encoded(expr, descr)
       String.build do |io|
         io << "if (#{expr} === null || #{expr} === undefined || typeof #{expr} !== \"object\" || typeof #{expr}.lat !== \"number\" || typeof #{expr}.lng !== \"number\") {\n"
-        io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"'\");\n"
+        io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"', expected #{class.name}, got '\" + #{expr} + \"'\");\n"
         io << "    typeCheckerError(err, ctx);\n"
         io << "}\n"
       end
