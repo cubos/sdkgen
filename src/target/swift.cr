4ffdb3a0f99318e7b244c882cf39c5f3f0d7e463
Tironi <igor@cubos.io>
2018-04-30 18:03:35 -0300

    added .copy property for structures

--- src/target/swift.cr
+++ src/target/swift.cr
@@ -33,13 +33,40 @@ abstract class SwiftTarget < Target
     native_type ref.type
   end
 
+  def generate_property_copy(t : AST::Type, path : String)
+    case t
+    when AST::StringPrimitiveType  ; path
+    when AST::IntPrimitiveType     ; path
+    when AST::UIntPrimitiveType    ; path
+    when AST::FloatPrimitiveType   ; path
+    when AST::DatePrimitiveType    ; path
+    when AST::DateTimePrimitiveType; path
+    when AST::BoolPrimitiveType    ; path
+    when AST::BytesPrimitiveType   ; path
+    when AST::ArrayType            ; "#{path}.map { #{generate_property_copy(t.base, "$0")} }"
+    when AST::EnumType             ; "#{native_type(t)}(rawValue: #{path}.rawValue)"
+    when AST::StructType           ; "#{path}.copy"
+    when AST::OptionalType         ; "#{path} != nil ? #{generate_property_copy(t.base, path + "!")} : nil"
+    when AST::TypeReference        ; generate_property_copy(t.type, path)
+    else
+      raise "BUG! Should handle primitive #{t.class}"
+    end
+  end
+
+
   def generate_struct_type(t)
     String.build do |io|
       io << "class #{t.name} {\n"
       t.fields.each do |field|
         io << ident "var #{field.name}: #{native_type field.type}\n"
       end
+      io << ident "\nvar copy: #{t.name} {\n"
+      io << ident ident "return #{t.name}(\n"
+      io << ident ident ident t.fields.map { |field| "#{field.name}: #{generate_property_copy(field.type, field.name)}"}.join(",\n")
+      io << ident ident "\n)\n"
+      io << ident "}\n"
       io << ident <<-END
+
 
 init() {
 
