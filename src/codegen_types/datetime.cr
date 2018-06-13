0ea7865c23b7ffead513f6cf37ede84e55b953e8
Guilherme Bernal <dev@lbguilherme.com>
2018-06-13 18:18:31 -0300

    accept preencoded datetime

--- src/codegen_types/datetime.cr
+++ src/codegen_types/datetime.cr
@@ -5,7 +5,7 @@ module AST
     end
 
     def typescript_encode(expr)
-      "#{expr}.toISOString().replace(\"Z\", \"\")"
+      "#{expr}.match(/^[0-9]{4}-[01][0-9]-[0123][0-9]T[012][0-9]:[0123456][0-9]:[0123456][0-9].[0-9]{3}$/) ? #{expr} : #{expr}.toISOString().replace(\"Z\", \"\")"
     end
 
     def typescript_native_type
@@ -29,7 +29,7 @@ module AST
 
     def typescript_check_decoded(expr, descr)
       String.build do |io|
-        io << "if (#{expr} === null || #{expr} === undefined || !(#{expr} instanceof Date)) {\n"
+        io << "if (#{expr} === null || #{expr} === undefined || !(#{expr} instanceof Date || #{expr}.match(/^[0-9]{4}-[01][0-9]-[0123][0-9]T[012][0-9]:[0123456][0-9]:[0123456][0-9].[0-9]{3}$/)) {\n"
         io << "    const err = new Error(\"Invalid Type at '\" + #{descr} + \"', expected #{self.class.name}, got '\" + #{expr} + \"'\");\n"
         io << "    typeCheckerError(err, ctx);\n"
         io << "}\n"
