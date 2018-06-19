ab15c475e1fb4689365eef0ef9861893f22a2b5c
Icaro Leite <icaaro.leite@gmail.com>
2018-06-18 22:43:53 -0300

    Better name to operations names and add forgotten declaration

--- src/semantic/check_multiple_declaration.cr
+++ src/semantic/check_multiple_declaration.cr
@@ -3,6 +3,7 @@ require "./visitor"
 module Semantic
   class CheckMultipleDeclaration < Visitor
     @names = Set(String).new
+    @op_names = Set(String).new
 
     def visit(definition : AST::TypeDefinition)
       if @names.includes? definition.name
@@ -13,10 +14,10 @@ module Semantic
     end
     
     def visit(op : AST::Operation)
-      if @operationsNames.includes? op.name
+      if @op_names.includes? op.name
         raise SemanticException.new("Function '#{op.name}' is declared multiples times")
       end
-      @operationsNames << op.name
+      @op_names << op.name
       super
     end
   end
