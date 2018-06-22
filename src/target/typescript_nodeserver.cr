38ad3c63ff8c2b2cbe317f9f804316c969237a51
davidcpires <dcpires06@gmail.com>
2018-06-22 11:52:16 -0300

    Added function name and decoded key to set cache (#34)
    * added function name and decoded key to set cache
    
    * added function name and decoded key to set cache
    
    * added function name and decoded key to set cache
    
    * stringfy the name of the funciton

--- src/target/typescript_nodeserver.cr
+++ src/target/typescript_nodeserver.cr
@@ -132,12 +132,12 @@ END
         @io << "\n"
       end
       @io << "\n"
-      @io << ident ident "let cacheKey: string | null = null, cacheExpirationSeconds: number | null = null, cacheVersion: number | null = null;\n"
+      @io << ident ident "let cacheKey: string | null = null, decodedKey: string | null = null, cacheExpirationSeconds: number | null = null, cacheVersion: number | null = null;\n"
       @io << ident ident "if (cacheConfig.#{op.pretty_name}) {\n"
       @io << ident ident ident "try {\n"
       @io << ident ident ident ident "const {key, expirationSeconds, version} = await cacheConfig.#{op.pretty_name}(#{(["ctx"] + op.args.map(&.name)).join(", ")});\n"
       @io << ident ident ident ident "if (!key) throw \"\";\n"
-      @io << ident ident ident ident "cacheKey = crypto.createHash(\"sha256\").update(JSON.stringify(key)+ \"-#{op.pretty_name}\").digest(\"hex\").substr(0, 100); cacheExpirationSeconds = expirationSeconds; cacheVersion = version;\n"
+      @io << ident ident ident ident "cacheKey = crypto.createHash(\"sha256\").update(JSON.stringify(key)+ \"-#{op.pretty_name}\").digest(\"hex\").substr(0, 100); decodedKey = JSON.stringify(key); cacheExpirationSeconds = expirationSeconds; cacheVersion = version;\n"
       @io << ident ident ident ident "const cache = await hook.getCache(cacheKey, version);console.log(JSON.stringify(cache));\n"
       @io << ident ident ident ident "if (cache && (!cache.expirationDate || cache.expirationDate > new Date())) return cache.ret;\n"
       @io << ident ident ident "} catch(e) {console.log(JSON.stringify(e));}\n"
@@ -145,7 +145,7 @@ END
       @io << ident ident "const ret = await fn.#{op.pretty_name}(#{(["ctx"] + op.args.map(&.name)).join(", ")});\n"
       @io << ident ident op.return_type.typescript_check_decoded("ret", "\"#{op.pretty_name}.ret\"")
       @io << ident ident "const encodedRet = " + op.return_type.typescript_encode("ret") + ";\n"
-      @io << ident ident "if (cacheKey !== null && cacheVersion !== null) hook.setCache(cacheKey, cacheExpirationSeconds ? new Date(new Date().getTime() + (cacheExpirationSeconds * 1000)) : null, cacheVersion, encodedRet);\n"
+      @io << ident ident "if (cacheKey !== null && cacheVersion !== null) hook.setCache(cacheKey, cacheExpirationSeconds ? new Date(new Date().getTime() + (cacheExpirationSeconds * 1000)) : null, cacheVersion, decodedKey!, \"#{op.pretty_name}\", encodedRet);\n"
       @io << ident ident "return encodedRet"
       @io << ident "},\n"
     end
@@ -225,7 +225,7 @@ export const hook: {
     onDevice: (id: string, deviceInfo: any) => Promise<void>
     onReceiveCall: (call: DBApiCall) => Promise<DBApiCall | void>
     afterProcessCall: (call: DBApiCall) => Promise<void>
-    setCache: (cacheKey: string, expirationDate: Date | null, version: number, ret: any) => Promise<void>
+    setCache: (cacheKey: string, expirationDate: Date | null, version: number, decodedKey: string, fnName: string, ret: any) => Promise<void>
     getCache: (cacheKey: string, version: number) => Promise<{expirationDate: Date | null, ret: any} | null>
 } = {
     onHealthCheck: async () => true,
