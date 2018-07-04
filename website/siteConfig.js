c40c48f1e74e7d5ab6dc6a59b17bfa91129f4780
Rodrigo Araujo <dygufa@gmail.com>
2018-07-03 23:07:54 -0300

    implementing ci & cd for docusaurus

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site' /* title for your website */,
+  title: 'Test Site 3' /* title for your website */,
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  url: 'https://cubos.github.io' /* your website url */,
+  baseUrl: '/sdkgen/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'sdkgen',
+  organizationName: 'cubos',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
