a4866f5620808d2d69ee74d6f6b09f1bb6a330a7
Victor Magalhães <victorhfmag@gmail.com>
2018-09-24 14:50:31 +0000

    Add tags per version with branch name (#37)

--- target-web/build_and_publish.sh
+++ target-web/build_and_publish.sh
@@ -10,6 +10,7 @@ cd /root/stage
 export PATH=$(npm bin):$PATH
 json -I -f package.json -e 'this.name="@cubos/'$NAME'"'
 npm version $VERSION || true
+npm dist-tag add @cubos/$NAME@$CI_COMMIT_REF_NAME || true
 echo "[tsc] START"
 tsc
 echo "[tsc] END"
