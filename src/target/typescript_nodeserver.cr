af280554fe1fdbed3a1aff2ce3d4709f24764f58
Rodrigo Araujo <dygufa@gmail.com>
2019-01-24 13:47:01 -0200

    updating hash algorithm for localtunnel subdomain generation

--- src/target/typescript_nodeserver.cr
+++ src/target/typescript_nodeserver.cr
@@ -446,7 +446,7 @@ export function start(port: number = 8000) {
     }
 
     if (process.env.DEBUGGING && !process.env.NOLOCALTUNNEL) {
-        const subdomain = require("crypto").createHash("md5").update(process.argv[1]).digest("hex").substr(0, 8);
+        const subdomain = require("crypto").createHash("sha256").update(process.argv[1]).digest("hex").substr(0, 8);
         require("localtunnel")(port, {subdomain}, (err: Error | null, tunnel: any) => {
             if (err) throw err;
             console.log("Tunnel URL:", tunnel.url);
