321a06eed548d45635c548ff50629b0eca45d3a2
Victor <victtavares1@gmail.com>
2019-02-22 19:20:26 -0300

    feature - change sdkgen to work wih mappable. (#45)

--- src/target/swift_ios.cr
+++ src/target/swift_ios.cr
@@ -9,10 +9,34 @@ import KeychainSwift
 class API {
     static var customUrl: String?
     static var useStaging = false
-    static var globalCallback: (_ method: String, _ result: APIInternal.Result<Any?>, _ callback: @escaping ((APIInternal.Result<Any?>) -> Void)) -> Void = { _, result, callback in
-        callback(result)
+    static var globalCallback: (_ method: String, _ result: ApiInternal.Result<Any?>, _ callback: ((ApiInternal.Result<Any?>) -> Void)?) -> Void = { _, result, callback in
+        callback?(result)
     }
 
+    static var isEnabledAssertion = true
+
+    static var apiInternal = ApiInternal()
+
+    static var decoder: JSONDecoder = {
+        let currentDecoder = JSONDecoder()
+        currentDecoder.dateDecodingStrategy = .custom({ (decoder) -> Date in
+            let container = try decoder.singleValueContainer()
+            let dateStr = try container.decode(String.self)
+
+            var date: Date? = apiInternal.decodeDate(str: dateStr) ?? apiInternal.decodeDateTime(str: dateStr)
+
+            guard let unwrapDate = date else {
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode date string \\(dateStr)")
+            }
+
+            return unwrapDate
+        })
+
+        return currentDecoder
+    }()
+
+     // MARK: Struct and Enums
+     struct NoReply: Codable {}\n\n
 END
 
     @ast.struct_types.each do |t|
@@ -29,10 +53,10 @@ END
       args = op.args.map { |arg| "#{arg.name}: #{native_type arg.type}" }
 
       if op.return_type.is_a? AST::VoidPrimitiveType
-        args << "callback: ((_ result: APIInternal.Result<Void>) -> Void)?"
+        args << "callback: ((_ result: ApiInternal.Result<NoReply>) -> Void)?"
       else
         ret = op.return_type
-        args << "callback: ((_ result: APIInternal.Result<#{native_type ret}>) -> Void)?"
+        args << "callback: ((_ result: ApiInternal.Result<#{native_type ret}>) -> Void)?"
       end
       @io << ident(String.build do |io|
         io << "@discardableResult\n"
@@ -45,34 +69,10 @@ END
           end
 
           op.args.each do |arg|
-            io << "args[\"#{arg.name}\"] = #{type_to_json arg.type, arg.name}\n\n"
+            io << "args[\"#{arg.name}\"] = #{type_to_json arg.type, arg.name}\n"
           end
-
-          io << "return APIInternal.makeRequest(#{op.pretty_name.inspect}, args) { response in \n"
-
-          io << ident(String.build do |io|
-            io << "switch response {\n"
-            io << <<-END
-            case .failure(let error):
-                callback?(APIInternal.Result.failure(error))\n
-            END
-
-            if op.return_type.is_a? AST::VoidPrimitiveType
-              io << <<-END
-              case .success:
-                  callback?(APIInternal.Result.success(()))\n
-              END
-            else
-              io << <<-END
-              case .success(let value):
-                  let returnObject = #{type_from_json(op.return_type, "value")}
-                  callback?(APIInternal.Result.success(returnObject))\n
-              END
-            end
-            io << "}\n"
-          end) # end of make request body indentation.
-
-          io << "}\n\n"
+          io << "\n"
+          io << "return apiInternal.makeRequest(#{op.pretty_name.inspect}, args, callback: callback)\n"
         end)
         io << "}"
       end)
@@ -82,10 +82,42 @@ END
     @io << <<-END
 }
 
-class APIInternal {
-    static var baseUrl = #{@ast.options.url.inspect}
+class ApiInternal {
+    var baseUrl = #{@ast.options.url.inspect}
 
-    private static let dateAndTimeFormatter: DateFormatter = {
+    // MARK: ApiInternal Inner classes
+    enum Result<T> {
+        case success(T)
+        case failure(Error)
+    }
+
+    class Error: Codable {
+        var type: API.ErrorType
+        var message: String
+
+        init(type: API.ErrorType, message: String) {
+            self.type = type
+            self.message = message
+        }
+    }
+
+    class HttpResponse<T: Codable>: Codable {
+        var ok: Bool
+        var deviceId: String
+        var result: T?
+        var error: Error?
+
+        required init(from decoder: Decoder) throws {
+            let values = try decoder.container(keyedBy: CodingKeys.self)
+            ok = try values.decode(Bool.self, forKey: .ok)
+            deviceId = try values.decode(String.self, forKey: .deviceId)
+            result = (try? values.decode(T.self, forKey: .result)) ?? (API.NoReply() as? T)
+            error = try? values.decode(Error.self, forKey: .error)
+        }
+    }
+
+    // MARK: ApiInternal Date Handler
+    let dateAndTimeFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .gregorian)
         formatter.timeZone = TimeZone(abbreviation: "UTC")
@@ -94,7 +126,7 @@ class APIInternal {
         return formatter
     }()
 
-    private static let dateFormatter: DateFormatter = {
+    let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .gregorian)
         formatter.locale = Locale(identifier: "en_US_POSIX")
@@ -102,40 +134,24 @@ class APIInternal {
         return formatter
     }()
 
-    enum Result<T> {
-        case success(T)
-        case failure(Error)
-    }
-    class Error {
-        var type: API.ErrorType
-        var message: String
-
-        init(_ type: API.ErrorType, _ message: String) {
-            self.type = type
-            self.message = message
-        }
-
-        init(json: [String: Any]) {
-            self.type = API.ErrorType(rawValue: json["type"] as! String) ?? API.ErrorType.Fatal
-            self.message = json["message"] as! String
-        }
+    func decodeDate(str: String) -> Date? {
+        return dateFormatter.date(from: str)
     }
 
-    class HTTPResponse {
-        var ok: Bool!
-        var deviceId: String!
-        var result: Any?
-        var error: Error?
-
-        init(json: [String: Any]) {
-            self.ok = (json["ok"] as! Bool)
-            self.deviceId = (json["deviceId"] as! String)
-            self.result = json["result"] is NSNull ? nil : json["result"]!
-            self.error = json["error"] is NSNull ? nil: (Error(json: json["error"] as! [String: Any]))
-        }
+    func encodeDate(date: Date) -> String {
+        return dateFormatter.string(from: date)
     }
 
-    static func device() -> [String: Any] {
+    func decodeDateTime(str: String) -> Date? {
+        return dateAndTimeFormatter.date(from: str)
+    }
+
+    func encodeDateTime(date: Date) -> String {
+        return dateAndTimeFormatter.string(from: date)
+    }
+
+    // MARK: ApiInternal Device Handler
+    func device() -> [String: Any] {
         var device = [String: Any]()
         device["platform"] = "ios"
         device["fingerprint"] = phoneFingerprint()
@@ -145,49 +161,31 @@ class APIInternal {
         } else {
             device["version"] = "unknown"
         }
+
         device["language"] = Locale.preferredLanguages[0]
-        if let deviceId = deviceID  {
-            device["id"] = deviceId
+        if let currentId = deviceId {
+            device["id"] = currentId
         }
+
         return device
     }
 
-    static func randomBytesHex(len: Int) -> String {
+    func randomBytesHex(len: Int) -> String {
         var randomBytes = [UInt8](repeating: 0, count: len)
         let _ = SecRandomCopyBytes(kSecRandomDefault, len, &randomBytes)
         return randomBytes.map({String(format: "%02hhx", $0)}).joined(separator: "")
     }
 
-    static func decodeDate(str: String) -> Date {
-        return dateFormatter.date(from: str) ?? Date()
-    }
-
-    static func encodeDate(date: Date) -> String {
-        return dateFormatter.string(from: date)
-    }
-
-    static func decodeDateTime(str: String) -> Date {
-        return dateAndTimeFormatter.date(from: str) ?? Date()
-    }
-
-    static func encodeDateTime(date: Date) -> String {
-        return dateAndTimeFormatter.string(from: date)
-    }
-
-    static var deviceID: String? {
+    var deviceId: String? {
         return UserDefaults.standard.value(forKey: "device-id") as? String
     }
 
-    static func saveDeviceID(_ id: String) {
+    func saveDeviceId(_ id: String) {
         UserDefaults.standard.setValue(id, forKey: "device-id")
         UserDefaults.standard.synchronize()
     }
 
-    static func isNull(value: Any?) -> Bool {
-        return value == nil || value is NSNull
-    }
-
-    static func phoneFingerprint() -> String {
+    func phoneFingerprint() -> String {
         let keychain = KeychainSwift()
         guard let phoneFingerprint = keychain.get("phoneFingerprint") else {
             let newPhoneFingerprint = randomBytesHex(len: 32)
@@ -198,8 +196,40 @@ class APIInternal {
         return phoneFingerprint
     }
 
+    // MARK: ApiInternal request functions
+    func globalCallback<T: Codable>(result: Result<T>, method: String, callback: ((Result<T>) -> Void)?) {
+
+        //Convert result to result of type Any
+        let anyResult: Result<Any?>
+        switch result {
+        case .success(let value):
+            anyResult = Result.success(value as Any?)
+        case .failure(let error):
+            anyResult = Result.failure(error)
+        }
+
+        API.globalCallback(method, anyResult) { result in
+            //Convert result to result of type T.
+            let tResult: Result<T>
+            switch result {
+            case .success(let value):
+
+                if let tValue = value as? T {
+                    tResult = Result.success(tValue)
+                } else {
+                    tResult = Result.failure(Error(type: API.ErrorType.Serialization,message: "Erro ao mapear os dados, tente novamente mais tarde"))
+                }
+
+            case .failure(let error):
+                tResult = Result.failure(error)
+            }
+
+            callback?(tResult)
+        }
+    }
+
     @discardableResult
-    static func makeRequest(_ name: String, _ args: [String: Any], callback: @escaping (Result<Any?>) -> Void) -> DataRequest {
+    func makeRequest<T: Codable>(_ name: String, _ args: [String: Any], callback: ((Result<T>) -> Void)?) -> DataRequest {
         let api = SessionManager.default
 
         let body = [
@@ -216,22 +246,34 @@ class APIInternal {
             method: .post,
             parameters: body,
             encoding: JSONEncoding.default
-           ).responseJSON { response in
-            guard let responseValue = response.result.value else {
-                let error = Error(API.ErrorType.Connection, "Erro de Conexão, tente novamente mais tarde")
-                API.globalCallback(name, Result.failure(error), callback)
-                return
+           ).responseData { [weak self] response in
+            guard let this = self else { return }
+
+            let responseResult: Result<T>
+            switch response.result {
+            case .success(let data):
+
+                do {
+                    let response = try API.decoder.decode(HttpResponse<T>.self, from: data)
+                    this.saveDeviceId(response.deviceId)
+                    if let result = response.result, response.ok {
+                        responseResult = Result.success(result)
+                    } else if let error = response.error {
+                        responseResult = Result.failure(error)
+                    } else {
+                        responseResult = Result.failure(Error(type: API.ErrorType.Serialization, message: "Algo de errado está acontecendo em nosso servidor, tente novamente mais tarde"))
+                    }
+
+                } catch let error {
+                    if API.isEnabledAssertion { assert(false, "Erro ao serializar dados, error: \\(error)") }
+                    responseResult = Result.failure(Error(type: API.ErrorType.Serialization, message: "Erro ao carregar seus dados, tente novamente mais tarde"))
+                }
+
+            case .failure:
+                responseResult = Result.failure(Error(type: API.ErrorType.Connection, message: "Erro de Conexão, tente novamente mais tarde"))
             }
 
-            let response = HTTPResponse(json: responseValue as! [String: Any])
-            saveDeviceID(response.deviceId)
-
-            guard response.error == nil && response.ok else {
-                API.globalCallback(name, Result.failure(response.error!), callback)
-                return
-            }
-
-            API.globalCallback(name, Result.success(response.result), callback)
+            this.globalCallback(result: responseResult, method: name, callback: callback)
         }
     }
 }
