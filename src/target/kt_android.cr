16dc53dda2ebf0aac435f780c8381360076fcb1a
Daniel Santiago <danielpsantiagodanielpsantiago@gmail.com>
2019-04-28 01:14:32 -0300

    add -in- word in kt mangle

--- src/target/kt_android.cr
+++ src/target/kt_android.cr
@@ -4,7 +4,7 @@ require "random/secure"
 class KtAndroidTarget < Target
   def mangle(ident)
     if %w[
-         as as? break class continue do else false
+         in as as? break class continue do else false
          for fun if in !in interface is !is null object
          package return super this throw true try typealias
          val var when while by catch constructor delegate
